76d9d83b81bf4e868983d0aea3e9d8d9f771f22d
Krish Shah <krishcshah09@gmail.com>
2026-04-23 00:46:00 +0530

    added put departments route

--- backend/controllers/eachDepartment.js
+++ backend/controllers/eachDepartment.js
@@ -13,3 +13,32 @@ export async function deleteDepartment(req,res){
         res.status(500).json({error:"Internal server error"})
     }
 }
+export async function putDepartment(req,res){
+    try{
+        const paramsId=req.params.id
+        let {name}=req.body
+        if(!name){
+            return res.status(400).json({error:"Missing name parameter"})
+        }
+        name=name.trim().toLowerCase()
+        if(!name){
+            return res.status(400).json({error:"Name cannot be empty"})
+        }
+        const isId=await db.query(`SELECT 1 FROM departments WHERE id=$1`,[paramsId])
+        if(isId.rows.length===0){
+            return res.status(404).json({error:"Department not found"})
+        }
+        const isName=await db.query(`SELECT 1 FROM departments WHERE name=$1`,[name])
+        if(isName.rows.length>0){
+            return res.status(409).json({error:"Department already exists"})
+        }
+        const newDepartment=await db.query(`UPDATE departments SET name=$1 WHERE id=$2 RETURNING name`,[name,paramsId])
+        const {name:newName}=newDepartment.rows[0]
+        res.status(200).json({
+            department:{id:paramsId,name:newName}
+        })
+    }catch(err){
+        console.log(err)
+        res.status(500).json({error:"Internal server error"})
+    }
+}
--- backend/routers/departmentsRouter.js
+++ backend/routers/departmentsRouter.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import {isAdmin} from '../middleware/isAdmin.js' 
 import {postDepartment,getDepartments} from '../controllers/allDepartmentsController.js'
-import {deleteDepartment} from '../controllers/eachDepartment.js'
+import {deleteDepartment,putDepartment} from '../controllers/eachDepartment.js'
 
 const router=express.Router()
 
 router.post('/',isAdmin,postDepartment)
 router.get('/',getDepartments)
 router.delete('/:id',isAdmin,deleteDepartment)
+router.put('/:id',isAdmin,putDepartment)
 
 export default router
