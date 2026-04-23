49ed3125413ffb7775dd319cd34032e507408bf3
Krish Shah <krishcshah09@gmail.com>
2026-04-23 11:57:34 +0530

    added cors + backend complete

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ import authRouter from './routers/authRouter.js'
 import employeesRouter from './routers/employeesRouter.js'
 import departmentsRouter from './routers/departmentsRouter.js'
 import {authMiddleware} from './middleware/authMiddleware.js'
+import cors from 'cors'
  
 dotenv.config()
 
 const app=express()
 app.use(express.json())
+app.use(cors())
 app.use('/auth',authRouter)
 app.use('/employees',authMiddleware,employeesRouter)
 app.use('/departments',authMiddleware,departmentsRouter)
